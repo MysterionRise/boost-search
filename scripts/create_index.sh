f171871092537c0611b5f95aee0c9ad18ed92f3e
Konstantin Perikov <pericov.ko@gmail.com>
2020-04-19 20:30:25 +0100

    add readme, update docker compose

---
+++ scripts/create_index.sh
@@ -0,0 +1,24 @@
+curl -X DELETE "localhost:9200/pdfs"
+
+curl -X PUT "localhost:9200/pdfs?pretty" -H 'Content-Type: application/json' -d'
+{
+  "mappings": {
+    "properties": {
+      "sentence_emb": {
+        "type": "dense_vector",
+        "dims": 768
+
+      },
+      "sentence_text" : {
+        "type" : "text",
+        "fields" : {
+          "keyword" : {
+            "type" : "keyword",
+            "ignore_above" : 256
+          }
+        }
+      }
+    }
+  }
+}
+'
