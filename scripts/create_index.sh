89135d212be93fdaec8eb750fdb0165f94556bcd
Konstantin Perikov <pericov.ko@gmail.com>
2020-05-05 23:26:25 +0100

    add knn settings and search

--- scripts/create_index.sh
+++ scripts/create_index.sh
@@ -40,3 +40,46 @@ curl -X PUT "localhost:9200/pdfs?pretty" -H 'Content-Type: application/json' -d'
   }
 }
 '
+
+curl -X PUT "localhost:9200/pdfs?pretty" -H 'Content-Type: application/json' -d'
+{
+  "settings": {
+    "index.knn": true
+  },
+  "mappings": {
+    "properties": {
+      "sentence_emb": {
+        "type": "knn_vector",
+        "dimension": 768
+      },
+      "sentence_text": {
+        "type": "text",
+        "fields": {
+          "keyword": {
+            "type": "keyword",
+            "ignore_above": 256
+          }
+        }
+      },
+      "document_name": {
+        "type": "text",
+        "fields": {
+          "keyword": {
+            "type": "keyword",
+            "ignore_above": 256
+          }
+        }
+      },
+      "chapter_name": {
+        "type": "text",
+        "fields": {
+          "keyword": {
+            "type": "keyword",
+            "ignore_above": 256
+          }
+        }
+      }
+    }
+  }
+}
+'
